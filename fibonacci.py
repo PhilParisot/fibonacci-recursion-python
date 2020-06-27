525f6e8240f299e1133b4379b60be00b84157c1a
pparisot <pparisot@pop-os.localdomain>
2020-06-26 21:21:29 -0400

    Simple fibonacci function

--- fibonacci.py
+++ fibonacci.py
@@ -1,2 +1,14 @@
+def display_fib(n: int):
+    n = int(n)
+
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return display_fib(n - 1) + display_fib(n - 2)
 
 
+Num = input("Input fibonacci number: ")
+
+print("Your fibonnaci number is ", display_fib(Num))
